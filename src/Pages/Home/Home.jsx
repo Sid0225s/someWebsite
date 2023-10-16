8d57ac3cdbaab7b4b933e85f9308fab4b29b0d13
Sidhant Sadyal <116481420+Sid0225s@users.noreply.github.com>
2023-10-16 13:54:48 +0530

    changes

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import front from "../../Media/hometheme.jpg"
+// import "./Home.css"
+
+function Home() {
+  return (
+    <div id='/'  className='home'>Home
+    </div>
+  )
+}
+
+export default Home
